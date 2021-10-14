f1b6ebcf24f5ab756293cdee370dedca8331edc0
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-10-14 23:19:48 +0200

    fixed the margin top of the cards in the menu page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -4,19 +4,17 @@ import { Link } from "react-router-dom";
 
 export default function AdminPage() {
   const { products } = useProduct();
-  
-  
+
   const Rows = products.map((item, index) => (
     <tr key={index}>
       <td>{item.name}</td>
       <td>{item.price}</td>
       <td>{item.category}</td>
-      
     </tr>
   ));
 
   return (
-    <div className='admin-page'>
+    <div className="admin-page">
       <h2>Products table</h2>
       <table>
         <thead>
